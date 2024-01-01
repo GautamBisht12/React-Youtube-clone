39fbd607c077dd43db0e885102ad7c2bd2a35033
Gautam Bisht <141318426+GautamBisht12@users.noreply.github.com>
2024-01-01 21:08:56 +0530

    Loader Added

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
+import {useState} from "react"
 
 const Videos = ({ videos, direction }) => {
+  const [loader , setLoader] = useState(true)
   if (!videos?.length) return "Loading...";
   console.log(videos);
-  return (
+  return !loader ? (
     <Stack
       direction={direction || "row"}
       flexWrap="wrap"
@@ -21,7 +23,7 @@ const Videos = ({ videos, direction }) => {
           </Box>
         ))}
     </Stack>
-  );
+  ) : (<span className="loader">Loading Videos</span>);
 };
 
 export default Videos;
